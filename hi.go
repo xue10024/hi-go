96f250e0161fef414ddba6dc524f9aeed1576993
xue10024 <tomcat02@sina.cn>
2018-03-15 16:41:18 +0800

    提交信息：第一次提交

---
+++ hi.go
@@ -0,0 +1,24 @@
+//程序包名
+package main
+//导入其他包
+import (
+	"fmt"
+	//"io"
+	//"time"
+	//"math"
+)
+//常量的定义
+const PI = 3.14
+//全局变量的声明与赋值
+var name = "gopher"
+//一般类型声明
+type newType int
+//结构的声明
+type goer struct{}
+//接口的声明
+type golan interface{}
+
+//由main函数作为程序主入口启动
+func main(){
+	fmt.Println("hello gooooo,狗")
+}
