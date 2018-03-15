1978afd491bb1e2b06e6b6a048829ed4b2f9c6bc
xue10024 <tomcat02@sina.cn>
2018-03-15 17:34:48 +0800

    提及信息：第二次提及

--- hi.go
+++ hi.go
@@ -1,5 +1,6 @@
 //程序包名
 package main
+
 //导入其他包
 import (
 	"fmt"
@@ -7,18 +8,23 @@ import (
 	//"time"
 	//"math"
 )
+
 //常量的定义
-const PI = 3.14
+const pI = 3.14
+
 //全局变量的声明与赋值
 var name = "gopher"
+
 //一般类型声明
 type newType int
+
 //结构的声明
 type goer struct{}
+
 //接口的声明
 type golan interface{}
 
 //由main函数作为程序主入口启动
-func main(){
-	fmt.Println("hello gooooo,狗")
+func main() {
+	fmt.Println("helloooo gooooo,狗")
 }
